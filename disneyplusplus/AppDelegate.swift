b1abe6b648703027ab5ca72ee186a435da1f55f1
Cameron Little <cameron@camlittle.com>
2025-03-01 19:27:46 -0700

    Initial Commit

---
+++ disneyplusplus/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  disneyplusplus
+//
+//  Created by Cameron Little on 2025-03-01.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
